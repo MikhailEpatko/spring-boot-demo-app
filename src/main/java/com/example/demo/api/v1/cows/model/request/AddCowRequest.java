70f7da9f67f274f474f9208f8105e3063f9e6240
Mikhail Epatko <m.epatko@jsa-group.ru>
2024-01-20 19:05:26 +0300

    [1.3] Исправил юнит-тесты

--- src/main/java/com/example/demo/api/v1/cows/model/request/AddCowRequest.java
+++ src/main/java/com/example/demo/api/v1/cows/model/request/AddCowRequest.java
@@ -7,10 +7,14 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class AddCowRequest {
 
     @NotNull(message = "Не может быть null")
