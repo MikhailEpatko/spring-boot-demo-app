e8c1b60646b1ff0dfdce838304f327cc908816f7
Mikhail Epatko <m.epatko@jsa-group.ru>
2024-02-02 11:57:06 +0300

    Добавил sonarqube

--- src/main/java/com/example/demo/common/errorhandler/ErrorHandlerController.java
+++ src/main/java/com/example/demo/common/errorhandler/ErrorHandlerController.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import static java.util.Collections.singletonList;
 
 @Slf4j
