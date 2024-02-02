574de4eaebc43f4a46cdb95422d8d96d23152eb8
Mikhail Epatko <m.epatko@jsa-group.ru>
2024-02-02 18:53:12 +0300

    ошибка checkstyle

--- src/main/java/com/example/demo/api/v1/cows/service/UpdateCow.java
+++ src/main/java/com/example/demo/api/v1/cows/service/UpdateCow.java
@@ -8,6 +8,7 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
