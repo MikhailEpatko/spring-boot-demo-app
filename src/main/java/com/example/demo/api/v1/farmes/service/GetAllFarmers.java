e6803b6697aa76a0d5bba020e23714bf2ad25866
EkaterinaLom <Lomakinaei@mail.ru>
2024-02-06 20:42:25 +1000

    [2.5] Поправила настройки IDEА, для корректного отбражения отступов и пробелов в импортах.

--- src/main/java/com/example/demo/api/v1/farmes/service/GetAllFarmers.java
+++ src/main/java/com/example/demo/api/v1/farmes/service/GetAllFarmers.java
@@ -3,11 +3,10 @@ package com.example.demo.api.v1.farmes.service;
 import com.example.demo.api.v1.farmes.model.entity.FarmerEntity;
 import com.example.demo.api.v1.farmes.repository.FarmerRepository;
 import jakarta.transaction.Transactional;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @Slf4j
